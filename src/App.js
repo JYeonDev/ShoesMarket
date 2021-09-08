3c6c1d616972f53c3b74b6e71b938843f82e83ac
JYeonDev <whddus4837@gmail.com>
2021-09-08 12:41:47 +0900

    슈즈마켓 만들기

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import "./App.css";
 import data from "./data";
 
+import { Link, Route, Switch } from "react-router-dom";
+
 function App() {
   let [shoes, setShoes] = useState(data);
   return (
@@ -40,46 +42,54 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <div className="jumbotron">
-        <h1>20% Season Off</h1>
-        <p>this is jumbotron</p>
-        <button>Click me!</button>
-      </div>
 
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes1.jpg"
-              width="100%"
-            />
-            <h4>{shoes[0].title}</h4>
-            <p>
-              {shoes[0].content} & {shoes[0].price}
-            </p>
-          </div>
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes2.jpg"
-              width="100%"
-            />
-            <h4>{shoes[1].title}</h4>
-            <p>
-              {shoes[1].content} & {shoes[1].price}
-            </p>
-          </div>
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes3.jpg"
-              width="100%"
-            />
-            <h4>{shoes[2].title}</h4>
-            <p>
-              {shoes[1].content} & {shoes[1].price}
-            </p>
+      <Route exact path="/">
+        <div className="jumbotron">
+          <h1>20% Season Off</h1>
+          <p>this is jumbotron</p>
+          <button>Click me!</button>
+        </div>
+        <div className="container">
+          {shoes.map((a, i) => {
+            return <Card shoes={a} i={i} key={i} />;
+          })}
+        </div>
+      </Route>
+      <Route path="/detail">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img
+                src="https://codingapple1.github.io/shop/shoes1.jpg"
+                width="100%"
+              />
+            </div>
+            <div className="col-md-6 mt-4">
+              <h4 className="pt-5">상품명</h4>
+              <p>상품설명</p>
+              <p>120000원</p>
+              <button className="btn btn-danger">주문하기</button>
+            </div>
           </div>
         </div>
-      </div>
+      </Route>
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        src={
+          "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
+        }
+        width="100%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>
+        {props.shoes.content} & {props.shoes.price}
+      </p>
     </div>
   );
 }
